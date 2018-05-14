7fb0b51c72ceda0c49ab6c327aff367c9e1f4b02
Abhishek Vyas <vyas6@purdue.edu>
2018-05-14 17:21:16 -0400

    Toggle header message on button click

--- index.js
+++ index.js
@@ -4,10 +4,23 @@
 //get element event occured on: ev.target
 
 const button = document.querySelector('button.greeting');
+const head1 = document.querySelector('#head1');
 
+let val = 0;
 
-function changeText(ev){
-    ev.target.textContent = 'Click Click';
+function changeText(){
+    val++;
+    if(val > 2){
+        val = 1;
+    }
+    
+    if(val == 1){
+        head1.textContent = 'Changed'; 
+    }
+    else if(val == 2){
+        head1.textContent = 'Unchanged'; 
+    }
+    
 }
 
 button.addEventListener('click', changeText);
