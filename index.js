f50964c1d63fed66e28a54202feeb2bb175facae
Abhishek Vyas <vyas6@purdue.edu>
2018-05-14 16:13:48 -0400

    Say yeah when megaButton is clicked

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('It works')
+//to get an id by querySelector use # before id
+//to get a class by querySelector use . before class
+const button = document.querySelector('button.greeting');
+
+function changeText(){
+    alert('yeah');
+}
+
+button.addEventListener('click', changeText);
