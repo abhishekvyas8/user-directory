7f10307fcc3ccee4f6eef19121f486e839ab4d73
Abhishek Vyas <vyas6@purdue.edu>
2018-05-15 16:04:56 -0400

    Display users in an unordered list

--- index.js
+++ index.js
@@ -11,16 +11,30 @@ const changeText = function(ev){
     const user = document.querySelector('#users');
     const userName = ev.target.userName.value;
     const age = ev.target.age.value
+    const favColor = ev.target.favoriteColor.value;
     //debugger
     if(userName == '' || age == ''){
         alert('Empty Field');
     }
     else{
         //user.innerHTML += `<p>${userName}, ${age}</p>`;
-        const p = document.createElement('p');
-        p.textContent = `${userName}, ${age}`;
-        user.appendChild(p);
-        p.style.backgroundColor = ev.target.favoriteColor.value;
+        const list = document.createElement('ul');
+
+        const nameItem = document.createElement('li');
+        nameItem.textContent = `Name: ${userName}`;
+        list.appendChild(nameItem);
+
+        const ageItem = document.createElement('li');
+        ageItem.textContent = `Age: ${age}`;
+        list.appendChild(ageItem);
+
+        const favColorItem = document.createElement('li');
+        favColorItem.textContent = `Favorite Color: ${favColor}`;   
+        list.appendChild(favColorItem);
+
+        user.appendChild(list);
+
+        favColorItem.style.color = favColor;
     } 
     
     ev.target.reset();
